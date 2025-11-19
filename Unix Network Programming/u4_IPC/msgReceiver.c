e81bfd64dc5749572680ae8f196c90d7461d9ace
abydym <1748185778@qq.com>
2025-11-19 16:38:17 +0800

    更新了好多东西嗯嗯:   msgReceiver.c 	这个也完善了好多捏:   msgSender.c 	../../.gitignore

--- Unix Network Programming/u4_IPC/msgReceiver.c
+++ Unix Network Programming/u4_IPC/msgReceiver.c
@@ -9,9 +9,16 @@ struct msg_st{
 	char buff[BUFSIZ];
 };
 
-int main(int argc, const char ** argv){
+int main(int argc, char ** argv){
 	struct msg_st msg;
-	long int msgtype = 0;
+	long int msgtype = 0;//这个是默认接受所有类型的消息
+	if(argc > 1){//这一块容易出错，首先argc是命令行里有几个参数，程序自己的名字就已经是一个参数了，如果没加其他参数然后直接去拿argv[1]，程序就会崩溃（Segmentation Fault，段错误），所以我们要先判断是否有大于1个参数，即argc > 1，然后再去捞，一定要防止越界。
+	msgtype = atol(argv[1]);
+	printf(">> Mode:Receiving only type %ld\n",msgtype);
+	}
+	else{
+	printf(">> Mode:Receiving ALL messages(FIFO)\n");
+	}
 
 	int msgid = msgget((key_t)5312, 0666 | IPC_CREAT);
 	if(msgid == -1){
--- Unix Network Programming/u4_IPC/msgSender.c
+++ Unix Network Programming/u4_IPC/msgSender.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <sys/msg.h>
 #include <errno.h>
+#include <string.h>
 
 struct msg_st{
 	long int msg_type;
@@ -12,7 +13,7 @@ struct msg_st{
 int main(){
 	struct msg_st msg;
 	char buff[BUFSIZ];
-	long int msgtype = 0;
+	//long int msgtype = 0; 这一行是僵尸代码，因为在之后根本没用上，主要还是依靠while循环里的msg.msg_type=1，因为消息队列的消息类型(mtype)要求是必须一个大于0的整数，如果为0了，msgsnd会报错，返回-1，errno设置为EINVAL。
 
 	int msgid = msgget((key_t)5312, 0666 | IPC_CREAT);
 	if(msgid == -1){
@@ -22,12 +23,20 @@ int main(){
 
 	while(1){
 		printf("Input message:");
-		fgets(buff, BUFSIZ, stdin);
-		buff[strlen(buff) - 1] = '\0';
-		msg.msg_type = 1;
+		fgets(buff, BUFSIZ, stdin); //键盘里的信息通过这个stdin来被送进buff
+		buff[strlen(buff) - 1] = '\0';//因为在最后会有一个换行符，所以这里使用这个来把最后的\n换成\0。
+		if(strncmp(buff, "urgent", 6) == 0){
+			msg.msg_type = 2;
+			printf(">>Sending URGENT message...\n");
+		}
+		else{
+			msg.msg_type = 1;
+			printf(">>Sending normal message...\n");
+		}
+		//现在尝试加入紧急消息和普通消息的机制，就不统一都用msg.msg_type = 1;
 		strcpy(msg.buff, buff);
 
-		if(msgsnd(msgid, (void *)&msg, BUFSIZ, 0) == -1){
+		if(msgsnd(msgid, (void *)&msg, strlen(buff) + 1, 0) == -1){//原来msgsnd的第三个参数是BUFSIZ，现在改成了strlen(buff)+1，即说明我发送可以只发送原来字符加上\0那么长，节省了很多资源，不用发8192B（BUFSIZ）的空间。
 			fprintf(stderr,"Msgsnd failed,errno:%s\n",strerror(errno));
 			return -1;
 		}
